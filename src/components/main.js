18ac1026a56c93b6bc278a756447e5f8c5bf48f4
JB Tellez <jb.tellez@gmail.com>
2023-04-11 12:01:31 -0700

    adds beast-judge start

---
+++ src/components/main.js
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+import Image from 'react-bootstrap/Image'
+
+class Main extends Component {
+    render() {
+      return (
+        <div>
+            <h2>{this.props.heading}</h2>
+            {
+                this.props.imageUrls.map(imageInfo => <HornedBeast info={imageInfo} />)
+            }
+        </div>
+      );
+    }
+  }
+
+  class HornedBeast extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: "Nay",
+        }
+    } 
+
+    // NOTE: use arrow functions for event handlers to retain proper contextual "this"
+    handleClick = () => {
+        // want to flip the yay to nay or vice versa
+        // in other words update state
+
+        const newStatus = this.state.status === "Nay" ? "Yay" : "Nay";
+
+        this.setState({
+            status: newStatus
+        });
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                <Image src={this.props.info.image_url} alt="some horned beast" rounded fluid />
+                <p>{this.props.info.title}</p>
+                <p>{this.state.status}</p>
+
+            </div>
+        );
+    }
+  }
+
+  export default Main;
